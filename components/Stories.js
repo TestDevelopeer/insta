fc8f9849cbda95148798579104c84b890b799af1
Igor Vereschinsky <testdeveloper7967@gmail.com>
2022-01-12 15:55:44 +0300

    Building the Stories Section 2/2

--- components/Stories.js
+++ components/Stories.js
@@ -4,7 +4,7 @@ import Story from "./Story";
 
 function Stories(props) {
     return (
-        <div className="">
+        <div className={'flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'}>
             {data.map(profile => (
                 <Story key={profile.id} img={profile.avatar} username={profile.username} />
             ))}
