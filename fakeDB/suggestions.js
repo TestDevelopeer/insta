350c8bc7f1d6a6f6249c69f672e1cdd85109885e
Igor Vereschinsky <testdeveloper7967@gmail.com>
2022-01-12 16:47:19 +0300

    Building the Suggestions component

---
+++ fakeDB/suggestions.js
@@ -0,0 +1,40 @@
+const suggestions = [
+    {
+        id: '1',
+        username: 'Iwan Swan',
+        avatar: '/fakeSuggestions/1.jpg',
+        company: {
+            name: 'VonRueden PLC'
+        }
+    },{
+        id: '2',
+        username: 'Josie Skinner',
+        avatar: '/fakeSuggestions/2.jpg',
+        company: {
+            name: 'Barton Ltd'
+        }
+    },{
+        id: '3',
+        username: 'Eden Charlton',
+        avatar: '/fakeSuggestions/3.jpg',
+        company: {
+            name: 'Lemke Group'
+        }
+    },{
+        id: '4',
+        username: 'Junaid Amin',
+        avatar: '/fakeSuggestions/4.jpg',
+        company: {
+            name: 'Watsica-Hansen'
+        }
+    },{
+        id: '5',
+        username: 'Musab Valdez',
+        avatar: '/fakeSuggestions/5.jpg',
+        company: {
+            name: 'Hagenes-Morar'
+        }
+    },
+];
+
+export default suggestions;
